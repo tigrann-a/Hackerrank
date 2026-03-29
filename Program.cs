3199ae54b6e5b9232b3f490e2a36d8cade124ec8
Tigran <taginian1991@hotmail.com>
2026-03-29 16:28:21 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿static string superReducedString(string s)
+{
+    for (int i = 0; i < s.Length; i++)
+    {
+        char current = s[i];
+
+        if(i + 1 <= s.Length - 1)
+        {
+            char next = s[i + 1];
+
+            if (current == next)
+            {
+                s = s.Remove(i, 2);
+                i = 0;
+            }
+        }
+    }
+
+    return s;
+}
+
+string s = "aaaabccddd";
+
+string s1 = superReducedString(s);
+Console.WriteLine(s1);
